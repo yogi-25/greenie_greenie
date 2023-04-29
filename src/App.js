ca58e3c2afeb17a262bac0dbdb24c9dfad74147b
yogita_misal <yogitamisal12@gmail.com>
2023-04-29 13:56:35 +0530

    added getJoke api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-
+import React from "react";
+import Axios from "axios";
 function App() {
+    const getJoke = ()=>
+    {
+        Axios.get("https://official-joke-api.appspot.com/random_joke").then((response)=> {console.log(response);})
+    }
   return (
     <div className="App">
-    </div>
-  );
-}
+        Hello Youtube <button onClick={getJoke}>get joke</button>
+    </div>);
 
+}
 export default App;
